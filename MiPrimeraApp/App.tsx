d0b1b6b8c7037cfa22b97ddff5a2a69b7a092f8e
Eriel Ramirez <eri3l2214@gmail.com>
2026-04-27 19:58:27 -0600

    Primer commit

---
+++ MiPrimeraApp/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Seven minutes, seven minutes is all i can spare to play with you</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
